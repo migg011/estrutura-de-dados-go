06b03996f38903040c1eefcc695dccc454987d36
migg011 <antoniommoutinho22@gmail.com>
2026-02-27 18:24:54 -0400

    atividade 1

--- Atividade 1 (aquecimento).go
+++ Atividade 1 (aquecimento).go
@@ -176,14 +176,8 @@ func questao8() {
 }
 
 func questao9() {
-	fmt.Print("1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765")
-} //kkkkkkk
-
-func questao09() {
 
 }
-
-//chatgpt a partir daqui
 
 func questao10() {
 
