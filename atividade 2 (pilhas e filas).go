756bb754499a88bdcd52525f0bb9b8d1d74a075b
migg011 <antoniommoutinho22@gmail.com>
2026-03-03 16:49:05 -0400

    atividade 02

---
+++ atividade 2 (pilhas e filas).go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+)
+
+var lista []int
+
+func subMenuLista() {
+	for {
+		var opcao int
+
+		fmt.Println("1) Inserir elemento na lista (Número aleatório)\n2) Remover elemento da lista\n3) Mostrar a cabeça (primeiro elemento)\n4) Mostrar o rabo (último elemento)\n-1) Voltar ao menu principal")
+		fmt.Scanln(&opcao)
+
+		switch opcao {
+		case 1:
+			logga("usuario escolheu adicionar um numero aleatorio ")
+			lista = append(lista, rand.Intn(100))
+			fmt.Println("lista atualizada: ", lista, "\n")
+		case 2:
+			if len(lista) > 0 {
+				logga("usuario escolheu remover um elemento da lista ")
+				lista = lista[:len(lista)-1]
+				fmt.Println("lista atualizada: ", lista, "\n")
+			} else {
+				fmt.Println("a lista esta vazia\n")
+				continue
+			}
+		case 3:
+			if len(lista) > 0 {
+				logga("usuario escolheu mostrar o primeiro elemento da lista")
+				primeiro := lista[0]
+				fmt.Println("o primeiro elemento da lista é: ", primeiro, "\n")
+			} else {
+				fmt.Println("a lista esta vazia\n")
+				continue
+			}
+		case 4:
+			if len(lista) > 0 {
+				logga("usuario escolheu mostrar o ultimo elemento da lista")
+				ultimo := lista[len(lista)-1]
+				fmt.Println("o ultimo elemento da lista é: ", ultimo, "\n")
+			} else {
+				fmt.Println("a lista esta vazia\n")
+				continue
+			}
+		case -1:
+			logga("usuario escolheu retornar ao menu principal")
+			return
+		default:
+			logga("usuario escolheu uma opcao invalida em menu de lista")
+			fmt.Println("opcao invalida")
+		}
+	}
+}
+
+func subMenuPilhas() {
+
+}
+
+func menuPrincipal() {
+
+	for {
+		var opcao int
+
+		fmt.Println("//1) Manipular Lista\n//2) Manipular Pilha\n//-1) Sair")
+		fmt.Scanln(&opcao)
+
+		switch opcao {
+		case 1:
+			logga("usuario escolheu manipular a lista")
+			subMenuLista()
+		case 2:
+			logga("usuario escolheu manipular a pilhas")
+			subMenuPilhas()
+		default:
+			logga("usuario escolheu sair do sistema")
+			fmt.Println("voce saiu do sistema")
+			break
+		}
+	}
+}
+
+func logga(msg string) {
+	file, _ := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	_, _ = file.WriteString("Timestamp: " + msg + "\n")
+}
+
+func main() {
+	menuPrincipal()
+}
